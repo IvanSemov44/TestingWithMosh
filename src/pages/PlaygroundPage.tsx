be7ce837c967bc53d9572cf1f4453cd485861636
IvanSemov44 <ivan.semov.44@gmail.com>
2024-11-04 16:35:55 +0200

    tested SearchBox

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,11 +1,12 @@
-import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
+import SearchBox from "../components/SearchBox";
 
 
 const PlaygroundPage = () => {
   // return <Onboarding />;
 
-  return <ExpandableText text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, magni corrupti laudantium quis voluptatem assumenda illum vero recusandae enim, atque non dolorum consectetur sequi? Mollitia impedit quasi, reiciendis et laboriosam non officia ipsa odit sint facere nihil, natus vero numquam eligendi enim dolore possimus distinctio veritatis iste dignissimos, saepe alias voluptates. Tempore fugit, velit maiores nihil odio delectus eveniet optio? Vel enim aliquam, sequi magnam alias quasi est corporis vitae sed officia eum facere distinctio maiores repellat expedita a sit dolor rem quam itaque. Voluptas pariatur omnis recusandae quasi eum vitae cupiditate rem dolores aspernatur voluptates. Expedita repudiandae quia ipsum." />;
+
+  return <SearchBox onChange={(text)=>console.log(text)}/>
 };
 
 export default PlaygroundPage;
